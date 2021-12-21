8acafe9bb141bfefe5bba481c70a78667f024d74
FlorentinTh <florentin.thullier1@uqac.ca>
2021-12-20 19:14:51 -0500

    feat(filehelper): add fileHelper component to better handle project related path of files

--- src/helpers/fileHelper.js
+++ src/helpers/fileHelper.js
@@ -1,9 +1,14 @@
 import url from 'url';
 import path from 'path';
-
+import fs from 'fs';
 class FileHelper {
-  static getPackageJson() {
+  static get __dirname() {
     return path.dirname(url.fileURLToPath(import.meta.url));
+  }
+
+  static getPackageJson() {
+    const filePath = path.join(this.__dirname, '..', '..', 'package.json');
+    return JSON.parse(fs.readFileSync(filePath, { encoding: 'utf-8' }));
   }
 }
 
